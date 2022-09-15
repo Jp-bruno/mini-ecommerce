5c7b6c8b869c8d4c982e6ea5daeaad292806b0a7
Joao <jp.bruno.machado@gmail.com>
2022-09-15 15:52:21 -0300

    teste

--- pages/api/checkout_sessions.ts
+++ pages/api/checkout_sessions.ts
@@ -3,49 +3,61 @@ import { NextApiRequest, NextApiResponse } from "next";
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body)
+  let lineItems;
+
   if (req.method === 'POST') {
     const itemsIDs = req.body.id;
 
     const itemsQuantity = req.body.quantity;
 
-    //aqui eu pego o preço de cada item a partir de suas ID's
-    const itemsPrices = await Promise.all(
-      itemsIDs.map( //aqui eu retorno uma array de promessas onde cada promessa vai me retornar o objeto price do produto desejado
-        async (el:string) => {
-          //primeiro eu procuro quais objetos price tem como produto o produto com o ID desejado
-          let item = await stripe.prices.search({
-            query: `product: "${el}"`
-          })
+    if (typeof itemsIDs === 'string') {
+      lineItems = await (async () => {
+        let item = await stripe.prices.search({
+          query: `product: "${itemsIDs}"`
+        })
 
-          return item.data[0]
+        return [{
+          price: item.data[0].id,
+          quantity: Number(itemsQuantity)
+        }]
+      })()
+
+      console.log(lineItems)
+
+    } else {
+      //aqui eu pego o preço de cada item a partir de suas ID's
+      const itemsPrices = await Promise.all(
+        itemsIDs.map( //aqui eu retorno uma array de promessas onde cada promessa vai me retornar o objeto price do produto desejado
+          async (productID: string) => {
+            let item = await stripe.prices.search({
+              query: `product: "${productID}"`
+            })
+
+            return item.data[0]
+          }
+        )
+      ).then(res => res.map(el => el.id));
+
+      lineItems = itemsQuantity.map((el: number, index: number) => {
+        return {
+          price: itemsPrices[index],
+          quantity: el,
         }
-      )
-    ).then(res => res.map(el => el.id));
-
-    const lineItems = itemsQuantity.map((el:number, index:number) => {
-      return {
-        price: itemsPrices[index],
-        quantity: el,
-      }
-    })
-
-
-    try {
-      const session = await stripe.checkout.sessions.create({
-        line_items: lineItems,
-        mode: 'payment',
-        success_url: `${req.headers.origin}/?success=true`,
-        cancel_url: `${req.headers.origin}/?canceled=true`,
-      });
-
-      res.redirect(303, session.url);
-    } catch (err) {
-      res.status((err as any).statusCode || 500).json((err as any).message);
+      })
     }
-  } else {
-    res.setHeader('Allow', 'POST');
-    res.status(405).end('Method Not Allowed');
   }
 
+
+  try {
+    const session = await stripe.checkout.sessions.create({
+      line_items: lineItems,
+      mode: 'payment',
+      success_url: `${req.headers.origin}/?success=true`,
+      cancel_url: `${req.headers.origin}/?canceled=true`,
+    });
+
+    res.redirect(303, session.url);
+  } catch (err) {
+    res.status((err as any).statusCode || 500).json((err as any).message);
+  }
 }
